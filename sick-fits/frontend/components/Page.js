8b28cffc6e7ff43884c90373de358943d0ad755a
jimmy-guzman <jimmy.guzman@outlook.com>
2018-10-14 14:05:38 -0500

    initial theme & layout w/ styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react'
 
 import Header from '../components/Header'
 import Meta from '../components/Meta'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  largeBr: '1300px'
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2 rem;
+`
 class Page extends Component {
   render () {
+    const { children } = this.props
+
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
